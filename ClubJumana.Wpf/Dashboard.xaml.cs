69ef85a63c6a3ba3a420c9a73077f88ac280e15c
mansour <smm.2020@gmail.com>
2020-06-07 10:49:48 -0400

    7 June

---
+++ ClubJumana.Wpf/Dashboard.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ClubJumana.Wpf.UserControls;
+
+namespace ClubJumana.Wpf
+{
+    /// <summary>
+    /// Interaction logic for Dashboard.xaml
+    /// </summary>
+    public partial class Dashboard : Window
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+            //TestBorder.Child=new ucTest();
+        }
+
+        private void BtnChange_OnClick(object sender, RoutedEventArgs e)
+        {
+           //TestBorder.Child=new ucTest2();
+        }
+    }
+}
---
+++ ClubJumana.Wpf/UserControls/ucCustomerCard.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClubJumana.Wpf.UserControls
+{
+    /// <summary>
+    /// Interaction logic for ucCustomerCard.xaml
+    /// </summary>
+    public partial class ucCustomerCard : UserControl
+    {
+        public ucCustomerCard()
+        {
+            InitializeComponent();
+        }
+    }
+}
