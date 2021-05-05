68350601ecd5cb745dfbc8cf95526c3ec88108fd
mansour <smm.2020@gmail.com>
2021-05-05 04:34:11 -0400

    may 5

---
+++ ClubJumana.Web/wwwroot/js/myJs.js
@@ -0,0 +1,6 @@
+﻿function mymessage(xx) {
+    var x = document.getElementById("snackbar");
+    x.innerHTML = xx;
+    x.className = "show";
+    setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
+}
