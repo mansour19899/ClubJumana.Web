7fce2ae0bb15c96df1dcdfc91952946aa930dd3b
mansour <smm.2020@gmail.com>
2021-05-09 02:48:12 -0400

    Refresh Page

--- ClubJumana.Web/wwwroot/js/myJs.js
+++ ClubJumana.Web/wwwroot/js/myJs.js
@@ -4,3 +4,22 @@
     x.className = "show";
     setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
 }
+
+let inactivityTime = function () {
+    let time;
+    window.onload = resetTimer;
+    document.onmousemove = resetTimer;
+    document.onkeypress = resetTimer;
+    function logout() {
+        document.getElementById("btnsessionExpird").click();
+    }
+    function resetTimer() {
+        clearTimeout(time);
+        time = setTimeout(logout, 900000)
+    }
+};
+window.onload = function () {
+    inactivityTime();
+}
+
+
