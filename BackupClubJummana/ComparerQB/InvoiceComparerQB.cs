53860041a63109e8f807397f9970d5e9de7d0199
mansour <smm.2020@gmail.com>
2021-07-14 00:26:52 -0400

    rr

---
+++ BackupClubJummana/ComparerQB/InvoiceComparerQB.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ClubJumana.DataLayer.Entities;
+
+namespace BackupClubJummana.ComparerQB
+{
+    class InvoiceComparerQB : IEqualityComparer<SaleOrder>
+    {
+        public bool Equals(SaleOrder x, SaleOrder y)
+        {
+            if (object.ReferenceEquals(x, y))
+            {
+                return true;
+            }
+
+
+            if (object.ReferenceEquals(x, null) || object.ReferenceEquals(y, null))
+            {
+                return false;
+            }
+
+
+            return x.Id == y.Id &&  x.Quantity == y.Quantity && x.LastUpdateTime == y.LastUpdateTime;
+        }
+
+        public int GetHashCode(SaleOrder obj)
+        {
+            if (obj == null)
+            {
+                return 0;
+            }
+            int IDHashCode = obj.Id.GetHashCode();
+
+            return IDHashCode;
+        }
+    }
+}
